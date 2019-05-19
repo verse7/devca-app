94ceb896f63968e8b1d3bd1b5f8bf1408270f9ff
Zidane Wright <zidanewright@outlook.com>
2019-05-19 11:52:47 -0500

    change database url

--- app/__init__.py
+++ app/__init__.py
@@ -6,7 +6,9 @@ app = Flask(__name__)
 csrf = CSRFProtect(app)
 app.config['UPLOAD_FOLDER'] = './app/static/uploads'
 app.config['SECRET_KEY'] = 'v\xf9\xf7\x11\x13\x18\xfaMYp\xed_\xe8\xc9w\x06\x8e\xf0f\xd2\xba\xfd\x8c\xda'
-DATABASE_URL = 'postgresql://admin:password123@localhost/devca'
+# DATABASE_URL = 'postgresql://admin:password123@localhost/devca'
+DATABASE_URL='postgres://dgkrabdgfaaxsv:cdb42fc6684be99aad789c19d3bdc1baa61a7e7f4b4c18b16aba85ac5743fb33@ec2-23-21-129-125.compute-1.amazonaws.com:5432/de5quhgjuh1m7d'
+
 app.config['SQLALCHEMY_DATABASE_URI'] = DATABASE_URL
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 
